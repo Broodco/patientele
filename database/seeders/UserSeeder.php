5b3db1cbcee81648b782f4d2a0f1ecbb4934ba55
Broodco <broodcoorens.maxime@gmail.com>
2022-05-21 12:35:35 +0200

    ADD UserSeeder and update DBSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         \App\Models\User::factory(10)->create();
+
+         \App\Models\User::factory()->create([
+             'name' => 'admin',
+             'email' => 'admin@patientele.com',
+             'password' => Hash::make('pass1234'),
+         ]);
+    }
+}
